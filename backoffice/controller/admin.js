e1cd77735bb4ae2d7763bfe2b1c0f0c8b5a1b149
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-02-29 23:47:38 +0100

    Header + Footer + Other improvements

--- backoffice/controller/admin.js
+++ backoffice/controller/admin.js
@@ -10,6 +10,13 @@ $(document).ready(function () {
         success: function (data) {
             $('#table').bootstrapTable({
                 data: data,
+                stickyHeader: true,
+                rowStyle: function rowStyle(value, row, index) {
+                    return {
+                        classes: 'text-nowrap another-class',
+                        css: {"font-size": "12px", "vertical-align" : "middle"}
+                    };
+                },
                 columns: [{
                     field: 'id',
                     title: "ID",
@@ -20,7 +27,8 @@ $(document).ready(function () {
                         field: 'createdAt',
                         title: "Fecha de publicaci&oacuten",
                         align: 'center',
-                        sortable: true
+                        sortable: true,
+                        formatter: fechaFormatter
 
                     }, {
                         field: 'label.name',
@@ -151,6 +159,11 @@ $(document).ready(function () {
         }
     });
 
+    $('.nav-refresh').on('click', function(){
+        refreshTable();
+        refreshMedia();
+    });
+
 });
 
 window.operateEvents = {
@@ -193,36 +206,40 @@ window.operateEvents = {
         }
 
         if(estadoPub){
-            publicAlerta = "Elemento Publicado";
+            publicAlerta = "Publicado";
             publicBtnTxt = "Des-Publicar";
             estiloBtnPub = 'btn-warning';
-            estiloPubAlert = "alert-success"
+            estiloPubAlert = "label-success"
         }else if(!estadoPub){
-            publicAlerta = "Elemento No Publicado";
+            publicAlerta = "No Publicado";
             publicBtnTxt = "Publicar";
             estiloBtnPub = 'btn-primary';
-            estiloPubAlert = "alert-warning";
+            estiloPubAlert = "label-warning";
         }
 
         if(estadoEdi){
-            editAlerta = "Elemento Editado";
-            estiloEdiAlert = "alert-success"
+            editAlerta = "Editado";
+            estiloEdiAlert = "label-success"
         }else if(!estadoEdi){
-            editAlerta = "Elemento No Editado";
-            estiloEdiAlert = "alert-warning"
+            editAlerta = "No Editado";
+            estiloEdiAlert = "label-warning"
         }
 
         if(row.photo!=null){
-            elemento= '<img style="width:auto; height:312px; max-width: 454px;" src="' + row.photo + '" href="' + row.photo + '"/>';
+            elemento= '<a href="' + row.photo + '" target="_blank" class="thumbnail" style="max-width: 50%; !important;"><img class="img-thumbnail" src="' + row.photo + '" href="' + row.photo + '"/></a>';
         }else{
             elemento= '<p>'+row.text+'</p>'
         }
 
         vex.open({
             content:
-            '<div class="container-fluid">' +
+            '<div class="container-fluid" style="height: inherit;">' +
                 '<div class="row">' +
-                    '<div class="col-md-4" style="text-align:left">' +
+                    '<div class="col-xs-6 col-md-3" style="text-align:left">' +
+                        '<h2>Elemento</h2>' +
+                            elemento +
+                    '</div>' +
+                    '<div class="col-xs-6 col-md-3 " style="text-align:left">' +
                         '<h2>Propiedades</h2>' +
                             '<h4>Categoría: </h4>' +
                                 '<select id="label" class="js-example-data-array">' +
@@ -250,27 +267,24 @@ window.operateEvents = {
                                 '<input id="nuevoMedio" type="text" style="width: 70%; margin-top:10px" class="hidden">' +
                             '<br>' +
                     '</div>' +
-                    '<div class="col-md-4" style="text-align:left">' +
-                        '<h2>Elemento</h2>' +
-                            elemento +
-                    '</div>' +
-                    '<div class="col-md-4" style="text-align:left">' +
+
+                    '<div class="col-xs-6 col-md-3" style="text-align:left">' +
                         '<h2 style="text-align: left">Estado</h2>' +
-                            '<h4>Publicado</h4>'+
-                            '<div class="alert '+estiloPubAlert+'" role="alert">'+publicAlerta+'</div>'+
-                            '<h4>Editado</h4>'+
-                            '<div class="alert '+estiloEdiAlert+'" role="alert">'+editAlerta+'</div>'+
-                            '<div class="row" style="margin-top: 50%;">' +
-                                '<div class="col-md-12" style="text-align: right">' +
-                                    '<button id="cancelar" type="button" class="btn btn-danger btn-lg" style="margin-right: 7px">Cancelar</button>' +
-                                    '<button id="publicar" type="button" class="btn '+estiloBtnPub+' btn-lg"style="margin-right: 7px">'+publicBtnTxt+'</button>' +
-                                    '<button id="guardar" type="button" class="btn btn-success btn-lg disabled">Guardar</button>' +
-                                '</div>' +
-                            '</div>' +
+                            '<h3><span class="label '+estiloPubAlert+'">'+publicAlerta+'</span></h3>'+
+                            '<h3><span class="label '+estiloEdiAlert+'">'+editAlerta+'</span></h3>'+
+
+                    '</div>' +
+                    '<div class="col-xs-6 col-md-3" style="text-align:left">' +
+                        '<h2 style="text-align: left">Acciones</h2>' +
+                        '<div class="btn-group-vertical" role="group" aria-label="...">'+
+                        '<button id="cancelar" type="button" class="btn btn-danger btn-lg" style="margin-right: 7px">Cancelar</button>' +
+                        '<button id="publicar" type="button" class="btn '+estiloBtnPub+' btn-lg"style="margin-right: 7px">'+publicBtnTxt+'</button>' +
+                        '<button id="guardar" type="button" class="btn btn-success btn-lg disabled">Guardar</button>' +
+                        '</div>'+
                     '</div>' +
                 '</div>' +
             '</div>',
-            contentCSS: {width: '90%', height: '100%'},
+            contentCSS: {width: '100%', height: '100%'},
 
             afterOpen: function ($vexContent) {
                 var nuevoMedio = false;
@@ -447,7 +461,8 @@ window.operateEvents = {
 
             },
             afterClose: function () {
-                window.location.reload();
+                refreshTable();
+                refreshMedia();
             }
         });
 
@@ -456,7 +471,7 @@ window.operateEvents = {
 
 
 function operateFormatter(index, row, element) {
-    return '<a class="btn opciones" href="#"><i class="fa fa-cogs"></i></button></a>';
+    return '<a class="btn opciones" href="#"><i class="fa fa-cogs fa-2x"></i></button></a>';
 }
 
 function imageFormatter(value, row) {
@@ -469,20 +484,138 @@ function imageFormatter(value, row) {
 
 function publishedFormatter(value, row) {
     if (row.published == true) {
-        return '<span class="glyphicon glyphicon-ok" style="color: green"/>';
+        return '<i class="fa fa-check fa-2x" style="color: green;"></i>';
     } else {
-        return '<span class="glyphicon glyphicon-remove" style="color: red"/>';
+        return '<i class="fa fa-times fa-2x" style="color: orange;"></i>';
     }
 }
 
 function editedFormatter(value, row) {
     if (row.edited == true) {
-        return '<span class="glyphicon glyphicon-ok" style="color: green"/>';
+        return '<i class="fa fa-check fa-2x" style="color: green;"></i>';
     } else {
-        return '<span class="glyphicon glyphicon-remove" style="color: red"/>';
+        return '<i class="fa fa-times fa-2x" style="color: orange"></i>';
     }
 }
 
+function fechaFormatter(value, row) {
+    var string = String(value);
+    var fecha = string.split("T");
+    return fecha[0];
+}
 
+function refreshTable(){
+    $.ajax({
+        type: "GET",
+        url: api + "Private/getContributionList",
+        headers: {
+            'Authorization': "Bearer " + localStorage.token
+        },
+        success: function (data) {
+            $('#table').bootstrapTable('destroy').bootstrapTable({
+                data: data,
+                stickyHeader: true,
+                rowStyle: function rowStyle(value, row, index) {
+                    return {
+                        classes: 'text-nowrap another-class',
+                        css: {"font-size": "12px", "vertical-align" : "middle"}
+                    };
+                },
+                columns: [{
+                    field: 'id',
+                    title: "ID",
+                    align: 'center',
+                    sortable: true
+                },
+                    {
+                        field: 'createdAt',
+                        title: "Fecha de publicaci&oacuten",
+                        align: 'center',
+                        sortable: true,
+                        formatter: fechaFormatter
+
+                    }, {
+                        field: 'label.name',
+                        title: 'Categoria',
+                        align: 'center',
+                        sortable: true
+
+
+                    }, {
+                        field: 'party.party',
+                        title: "Partido pol&iacutetico",
+                        align: 'center',
+                        sortable: true
+
+                    }, {
+                        field: 'location.name',
+                        title: "Municipio",
+                        align: 'center',
+                        sortable: true
+
+                    }, {
+                        field: 'media.media',
+                        title: 'Medios de comunicaci&oacuten',
+                        align: 'center',
+                        sortable: true
+
+                    }, {
+                        field: 'photo',
+                        title: 'Datos',
+                        align: 'center',
+                        sortable: false,
+                        formatter:imageFormatter
+                    }, {
+                        field: 'published',
+                        title: 'Publicado?',
+                        align: 'center',
+                        sortable: true,
+                        formatter: publishedFormatter
+
+                    }, {
+                        field: 'edited',
+                        title: 'Editado?',
+                        align: 'center',
+                        sortable: true,
+                        formatter: editedFormatter
+
+                    }, {
+                        field: 'operate',
+                        title: 'Opciones',
+                        align: 'center',
+                        sortable: false,
+                        events: operateEvents,
+                        formatter: operateFormatter
+                    }]
+            });
+        },
+        error: function (request, status, error) {
+            alert("Error al cargar la tabla.");
+            window.location.href = 'index.html';
+        }
+    });
+}
+
+function refreshMedia(){
+    sessionStorage.removeItem('mediaList');
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: api + "Private/getMediaList",
+        headers: {
+            'Authorization': "Bearer " + localStorage.token
+        },
+        success: function (data) {
+            var array = [];
+            for (var i = 0; i < data.length; i++) {
+                array.push({id: data[i].id, text: data[i].media});
+            }
+
+            sessionStorage.setItem('mediaList', JSON.stringify(array));
+
+        }
+    });
+
+}
 
 
